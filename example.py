5ac40b664fff645029dc444bfb68c09421f6f1f0
Mayank <mayank17.mewar@gmail.com>
2022-06-26 20:01:46 +0530

    using for loop

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
+i=0
 for i in range(0,10):
-    str="this is a demo for git{}"
+    str="this is a demo for git{} using for!"
     version=3.0
     print(str.format(version))
     print(f"{i+1}. hello mayank")
